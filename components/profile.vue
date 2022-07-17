0fb8b718c1e39d6a724690395a714e9bc2bdb211
Mathias Løken <lakavi@mailfence.com>
2022-07-17 13:31:50 +0200

    Prettier login modal

---
+++ components/profile.vue
@@ -0,0 +1,82 @@
+<template>
+	<WrapModal ref="modal">
+		<template v-slot:header>
+			<h5 class="text-xl">Profile</h5>
+		</template>
+		
+		<table class="table-auto">
+			<tbody>
+			<tr>
+				<td class="font-bold text-gray-300">Name</td>
+				<td class="pl-3">{{ user?.name }}</td>
+			</tr>
+			<tr>
+				<td class="font-bold text-gray-300">Email</td>
+				<td class="pl-3">{{ user?.email }}</td>
+			</tr>
+			</tbody>
+		</table>
+		
+		<template v-slot:footer>
+			
+			<div class="p-3 flex justify-end">
+				<button class="Btn Purple" type="button" v-on:click="logout">
+					Logout
+				</button>
+			</div>
+		</template>
+	</WrapModal>
+</template>
+
+<script lang="ts" setup>
+import { useUserManager } from "#imports";
+
+const user = useUserManager();
+</script>
+
+<script lang="ts">
+import { useRuntimeConfig } from "#app";
+
+export default {
+	name: "profile",
+	data()
+	{
+		return {};
+	},
+	expose: ["show", "hide", "toggle"],
+	methods: {
+		show(): void
+		{
+			this.$refs.modal.show();
+		},
+		hide(): void
+		{
+			this.$refs.modal.hide();
+		},
+		toggle(): void
+		{
+			this.$refs.modal.toggle();
+		},
+		async logout(): Promise<void>
+		{
+			const init: RequestInit = {
+				method: "POST",
+				credentials: "include"
+			};
+			const res = await fetch(useRuntimeConfig().public.BACKEND_URL + "/V1/Login/Logout", init);
+			if (res.ok)
+			{
+				window.location.replace("/");
+			}
+		}
+	}
+};
+</script>
+
+<style lang="postcss">
+
+</style>
+
+<style lang="postcss" scoped>
+
+</style>
---
+++ components/wrap/modal.vue
@@ -0,0 +1,60 @@
+<template>
+	<div>
+		<div :class="{hidden: !showModal}" class="fixed flex h-screen w-screen z-40">
+			<div class="m-auto Modal bg-gray-800 shadow-2xl rounded z-50">
+				<div class="p-3 border-b border-gray-700">
+					<slot name="header"></slot>
+				</div>
+				
+				<div class="p-3">
+					<slot></slot>
+				</div>
+				
+				<div class="p-3">
+					<slot name="footer"></slot>
+				</div>
+			</div>
+			<div class="absolute bg-opacity-30 bg-gray-900 w-full h-full" v-on:click="hide" />
+		</div>
+	</div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<script lang="ts">
+// noinspection JSUnusedGlobalSymbols
+export default {
+	name: "modal",
+	data()
+	{
+		return {
+			showModal: false
+		};
+	},
+	expose: ["show", "hide", "toggle"],
+	methods: {
+		show(): void
+		{
+			this.showModal = true;
+		},
+		hide(): void
+		{
+			this.showModal = false;
+		},
+		toggle(): void
+		{
+			this.showModal = !this.showModal;
+		}
+	}
+};
+</script>
+
+<style lang="postcss">
+
+</style>
+
+<style lang="postcss" scoped>
+
+</style>
